7ac4eea91d65a6561d4446a57880c58fedc2b5a4
Nitish Sarin <nitishhsarin@gmail.com>
2017-09-25 23:08:10 +0530

    Create head.php

---
+++ include/head.php
@@ -0,0 +1,10 @@
+<div class="header">
+    <div class="logo float-left">
+        <a class="logo" href='homepage.php'>O.C.A.G</a>
+    </div>
+    <div class="login-signup float-right">
+        <?php
+        include("include/navigation.php");
+        ?>
+    </div>
+</div>
